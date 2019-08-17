340c4220a5b3dd867057ee1d511ced7fd85318ee
Richard <rjmpham@gmail.com>
2019-08-17 11:43:47 -0600

    Added button handeler at Square level

--- src/View/Square.js
+++ src/View/Square.js
@@ -2,13 +2,20 @@ import React from "react";
 
 export default class Square extends React.Component{
 
+    displayLevel = this.props.gameState.getTile(this.props.position).topLevel;
+
+    handleClick(){
+        this.props.onClick(this.props.position);
+        this.displayLevel = this.props.gameState.getTile(this.props.position).topLevel;
+    }
+
     //pros.value is a Vector2 of the board position
     render(){
         return (
-            <button className="square" onClick={this.props.onClick(this.props.position)}>
+            <button className="square" onClick={() => this.handleClick()}>
                 <div className= "buildRow">   
                     <div className= "buildDisplay"> 
-                        {this.props.gameState.getTile(this.props.position).topLevel} 
+                        {this.displayLevel} 
                     </div>
                     <div className= "buildSpace"> </div>
                     <div className= "buildDisplay"> </div>
