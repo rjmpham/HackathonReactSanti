d6e851a3f625c6ee886c0fb3ce747d7fc22d57a4
Richard <rjmpham@gmail.com>
2019-08-30 15:08:44 -0600

    Added temp art

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Game from "./View/Game.js"
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// eslint-disable-next-line no-unused-vars
+import Game from './View/Game.js'
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
